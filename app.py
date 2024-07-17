4a74825f0c231f9633111a65b5cbc75286ac8f18
Kaushal845 <23110160@iitgn.ac.in>
2024-07-18 02:23:56 +0530

    DCC Assignment 4

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, request, Response, render_template, jsonify
 from flask_mysqldb import MySQL
+import requests
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = '127.0.0.1'
@@ -19,6 +20,17 @@ def indian_number(number):
 # Add the filter to the Jinja environment
 app.jinja_env.filters['indian_number'] = indian_number
 
+API_KEY = 'YOUR_GOOGLE_API_KEY'
+SEARCH_ENGINE_ID = 'YOUR_SEARCH_ENGINE_ID'
+
+def fetch_info(name):
+    search_url = f"https://www.googleapis.com/customsearch/v1?key=AIzaSyBxx1YHuQp2MlY_rW2pKPiPzCePX9rspQ0&cx=071a0f1918a4847ce&q={name}"
+    response = requests.get(search_url)
+    if response.status_code == 200:
+        search_results = response.json()
+        if 'items' in search_results:
+            return search_results['items'][0]  # Get the first result
+    return None
 
 @app.route('/')
 def index():
@@ -64,7 +76,9 @@ def nbonds():
             (q3,))
         records = cursor.fetchall()
         cursor.close()
-        return render_template('q1e2results.html', records=records)
+        company_info = fetch_info(q3)
+        cname = q3
+        return render_template('q1e2results.html', records=records, company_info=company_info, cname=cname)
     cursor = mysql.connection.cursor()
     cursor.execute('SELECT DISTINCT(Company) FROM companybond')
     name = cursor.fetchall()
@@ -85,7 +99,9 @@ def nbondsparty():
         party = [row[1] for row in records]
         bonds = [row[2] for row in records]
         tot = [row[3] for row in records]
-        return render_template('q1e3results.html', records=records)
+        pinfo = fetch_info(q4)
+        pname = q4
+        return render_template('q1e3results.html', records=records,pinfo=pinfo,pname=pname)
 
     cursor = mysql.connection.cursor()
     cursor.execute('SELECT DISTINCT(Party) FROM partybond ORDER BY Party')
@@ -107,7 +123,10 @@ def companyparty():
         # print(records)
         cursor.close()
         return render_template('q1e4result.html', records=records, combined_donation_amount=combined_donation_amount)
-    return render_template('q1e4.html')
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT DISTINCT(Company) FROM companybond ORDER BY Company')
+    name = cursor.fetchall()
+    return render_template('q1e4.html',names=name)
 
 
 @app.route('/partycompany', methods=['GET', 'POST'])
@@ -115,6 +134,8 @@ def partycompany():
     if request.method == 'POST':
         q6 = request.form.get('Partyname')
         cursor = mysql.connection.cursor()
+        cursor.execute('SELECT DISTINCT(Party) FROM partybond')
+        name = cursor.fetchall()
         cursor.execute(
             'SELECT Company,Bondnumber,Denominations FROM companybond WHERE Bondnumber IN (SELECT Bond_Number FROM partybond WHERE Party = %s)',
             (q6,))
@@ -123,7 +144,10 @@ def partycompany():
         combined_donation_amount = sum(int(row[2]) for row in records)
         cursor.close()
         return render_template('q1e5result.html', records=records, combined_donation_amount=combined_donation_amount)
-    return render_template('q1e5.html')
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT DISTINCT(Party) FROM partybond')
+    name = cursor.fetchall()
+    return render_template('q1e5.html',names=name)
 
 
 @app.route('/pie')
